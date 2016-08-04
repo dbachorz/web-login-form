029c84e0612c0a50b0e9551edb06f2267e810489
Damian Bachórz <damian.bachorz@briisk.co>
2016-08-05 00:06:30 +0200

    name refactor

--- signIn.php
+++ signIn.php
@@ -9,7 +9,6 @@
 
     require_once "connect.php";
     $connection = @new mysqli($host, $db_user, $db_password, $db_name);
-
     if($connection->connect_errno != 0) {
         echo "Error:".$connection->connect_errno;
     }else{
@@ -19,14 +18,14 @@
         $login = htmlentities($login, ENT_QUOTES, "UTF-8");
         $password = htmlentities($password, ENT_QUOTES, "UTF-8");
 
-        if($result = @$connection->query(sprintf("SELECT * FROM game_users WHERE user='%s' AND pass='%s'",
+        if($query_result = @$connection->query(sprintf("SELECT * FROM game_users WHERE user='%s' AND pass='%s'",
             mysqli_real_escape_string($connection, $login),
             mysqli_real_escape_string($connection, $password)))){
-            $usersCount = $result->num_rows;
+            $usersCount = $query_result->num_rows;
             if($usersCount == 1){
                 $_SESSION['logged'] = true;
                 echo "Logged in succesfuly";
-                $row = $result->fetch_assoc();
+                $row = $query_result->fetch_assoc();
                 $_SESSION['user'] = $row['user'];
                 $_SESSION['id'] = $row['id'];
                 $_SESSION['drewno'] = $row['drewno'];
@@ -35,7 +34,7 @@
                 $_SESSION['email'] = $row['email'];
                 $_SESSION['dnipremium'] = $row['dnipremium'];
                 unset($_SESSION['error']);
-                $result->free();
+                $query_result->free();
                 header('Location: game.php');
             }else{
                 $_SESSION['error'] = '<span style="color: red;">Wrong username or password</span>';
